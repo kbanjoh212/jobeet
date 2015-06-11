9db1fb8c0c147253be176b979230ebb8e6313303
KennyB <kbanjoh212@gmail.com>
2015-06-11 11:52:49 +0100

    Jobeet repository created
    Essential modules added

---
+++ lib/model/JobeetJob.php
@@ -0,0 +1,24 @@
+<?php
+class JobeetJob extends BaseJobeetJob
+{
+	public function __toString()
+	{
+		return sprintf('%s at %s (%s)', $this->getPosition(), $this->getCompany(), $this->getLocation());
+	}
+	  
+	  public function getCompanySlug()
+	  {
+	  	return Jobeet::slugify($this->getCompany());
+	  }
+	  
+	  public function getPositionSlug()
+	  {
+	  	return Jobeet::slugify($this->getPosition());
+	  }
+	  
+	  public function getLocationSlug()
+	  {
+	  	return Jobeet::slugify($this->getLocation());
+	  }
+  
+}
