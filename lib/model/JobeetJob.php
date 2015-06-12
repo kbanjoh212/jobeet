33013cb1f2dc6a3bdeae718ac81d93dcf7dd533f
KennyB <kbanjoh212@gmail.com>
2015-06-12 11:13:56 +0100

    Jroutes
    Now the data are now showing up in the database as met to be.

--- lib/model/JobeetJob.php
+++ lib/model/JobeetJob.php
@@ -1,31 +1,40 @@
 <?php
 class JobeetJob extends BaseJobeetJob
 {
-public function getActiveJobs()
-  {
-    $criteria = new Criteria();
-    $criteria->add(self::EXPIRES_AT, time(), Criteria::GREATER_THAN);
-	$criteria->addDescendingOrderByColumn(self::EXPIRES_AT);
- 
-    return self::doSelect($criteria);
-  }
-public function __toString()
-	{
-		return sprintf('%s at %s (%s)', $this->getPosition(), $this->getCompany(), $this->getLocation());
-	}
-	  
-public function getCompanySlug()
+	public function getActiveJobs()
 	  {
-	  	return Jobeet::slugify($this->getCompany());
+	    $criteria = new Criteria();
+	    $criteria->add(self::EXPIRES_AT, time(), Criteria::GREATER_THAN);
+		$criteria->addDescendingOrderByColumn(self::EXPIRES_AT);
+	 
+	    return self::doSelect($criteria);
 	  }
- public function getPositionSlug()
-	  {
-	  	return Jobeet::slugify($this->getPosition());
-	  }
-	  
-public function getLocationSlug()
-	  {
-	  	return Jobeet::slugify($this->getLocation());
-	  }
-
+	public function __toString()
+		{
+			return sprintf('%s at %s (%s)', $this->getPosition(), $this->getCompany(), $this->getLocation());
+		}
+		  
+	public function getCompanySlug()
+		  {
+		  	return Jobeet::slugify($this->getCompany());
+		  }
+	 public function getPositionSlug()
+		  {
+		  	return Jobeet::slugify($this->getPosition());
+		  }
+		  
+	public function getLocationSlug()
+		  {
+		  	return Jobeet::slugify($this->getLocation());
+		  }
+		  public function save(PropelPDO $con = null)
+		  {
+		  	if ($this->isNew() && !$this->getExpiresAt())
+		  	{
+		  		$now = $this->getCreatedAt() ? $this->getCreatedAt('U') : time();
+		  		$this->setExpiresAt($now + 86400 * sfConfig::get('app_active_days'));
+		  	}
+		  
+		  	return parent::save($con);
+		  }
 }
